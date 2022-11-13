7b514b910c860f7c678bbedfcfc0f3ded7e2c960
Dreamboy-26 <heyimpankaj2616@gmail.com>
2022-11-13 17:07:03 +0530

    no change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Figma from './component/Figma';
-import Part2 from './component/Part2';
+
 
 function App() {
   return (
     <div className="App">
      
     <Figma/>
-    {/* <Part2/> */}
+  
     </div>
   );
 }
